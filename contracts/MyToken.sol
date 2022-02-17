5dc474934fd663b1d1249f8abe170657d8cca348
Abiola <ajibolaabiolaahmed@gmail.com>
2022-02-17 01:47:27 +0100

    Initial Setup amd smart contracts

---
+++ contracts/MyToken.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("Chief Token", "CTK") {}
+}
